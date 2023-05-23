a5685a37c124432b58f0ff23d6287b0aabcb831a
Kirill Platonov <kirill@platonov.net>
2023-05-23 15:08:49 +0400

    Init plugin

---
+++ rdjson_formatter/rdjson_formatter.rb
@@ -0,0 +1,139 @@
+# frozen_string_literal: true
+
+# https://docs.rubocop.org/rubocop/formatters.html
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+class RdjsonFormatter < RuboCop::Formatter::BaseFormatter
+  include RuboCop::PathUtil
+
+  def started(_target_files)
+    @rdjson = {
+      source: {
+        name: "rubocop",
+        url: "https://rubocop.org/"
+      },
+      diagnostics: []
+    }
+    super
+  end
+
+  def file_finished(file, offenses)
+    offenses.each do |offense|
+      next if offense.location == RuboCop::Cop::Offense::NO_LOCATION
+
+      @rdjson[:diagnostics] << build_diagnostic(file, offense)
+    end
+  end
+
+  def finished(_inspected_files)
+    puts @rdjson.to_json
+  end
+
+  private
+
+  # @param [String] file
+  # @param [RuboCop::Cop::Offense] offense
+  # @return [Hash]
+  def build_diagnostic(file, offense)
+    code, message = offense.message.split(":", 2).map(&:strip)
+
+    diagnostic = {
+      message: message,
+      location: {
+        path: convert_path(file),
+        range: {
+          start: {
+            line: offense.location.begin.line,
+            column: offense.location.begin.column + 1
+          },
+          end: {
+            line: offense.location.end.line,
+            column: offense.location.end.column + 1
+          }
+        }
+      },
+      severity: convert_severity(offense.severity),
+      code: {
+        value: code
+      },
+      original_output: build_original_output(file, offense)
+    }
+
+    diagnostic[:suggestions] = build_suggestions(offense) if offense.correctable? && offense.corrector
+
+    diagnostic
+  end
+
+  # @param [RuboCop::Cop::Offense] offense
+  # @return [Array{Hash}]
+  def build_suggestions(offense)
+    range, text = offense.corrector.as_replacements[0]
+
+    [
+      {
+        range: {
+          start: {
+            line: range.begin.line,
+            column: range.begin.column + 1 # rubocop is 0-origin, reviewdog is 1-origin
+          },
+          end: {
+            line: range.end.line,
+            column: range.end.column + 1
+          }
+        },
+        text: text
+      }
+    ]
+  end
+
+  # https://github.com/reviewdog/reviewdog/blob/1d8f6d6897dcfa67c33a2ccdc2ea23a8cca96c8c/proto/rdf/reviewdog.proto
+  # https://docs.rubocop.org/rubocop/configuration.html#severity
+  #
+  # @param [Symbol] severity
+  # @return [String]
+  def convert_severity(severity)
+    case severity
+    when :info
+      "INFO"
+    when :warning
+      "WARNING"
+    when :error
+      "ERROR"
+    else
+      "UNKNOWN_SEVERITY"
+    end
+  end
+
+  # extract reasonable relative path from (ideally) the project root.
+  # if `path` is `"/path/to/project/lib/my_file.rb"`,
+  # it generates `"lib/my_file.rb"` as
+  #
+  # ref: rubocop approach
+  # https://github.com/rubocop/rubocop/blob/1e55b1aa5e4c5eaeccad5d61f08b7930ed6bc341/lib/rubocop/path_util.rb#L25
+  #
+  # @param [String] path
+  # @return [String]
+  def convert_path(path)
+    base_path = Dir.pwd
+
+    begin
+      Pathname.new(File.expand_path(path)).relative_path_from(base_path).to_s
+    rescue ArgumentError
+      path
+    end
+  end
+
+  # @param [String] file
+  # @param [RuboCop::Cop::Offense] offense
+  # @return [String]
+  def build_original_output(file, offense)
+    format(
+      "%<path>s:%<line>d:%<column>d: %<severity>s: %<message>s",
+      path: smart_path(file),
+      line: offense.line,
+      column: offense.real_column,
+      severity: offense.severity.code,
+      message: offense.message
+    )
+  end
+end
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength
---
+++ script.sh
@@ -0,0 +1,106 @@
+#!/bin/sh -e
+version() {
+  if [ -n "$1" ]; then
+    echo "-v $1"
+  fi
+}
+
+cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+TEMP_PATH="$(mktemp -d)"
+PATH="${TEMP_PATH}:$PATH"
+
+echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+echo '::endgroup::'
+
+if [ "${INPUT_SKIP_INSTALL}" = "false" ]; then
+  echo '::group:: Installing standard with plugins ... https://github.com/standardrb/standard'
+  # if 'gemfile' standard version selected
+  if [ "${INPUT_STANDARD_VERSION}" = "gemfile" ]; then
+    # if Gemfile.lock is here
+    if [ -f 'Gemfile.lock' ]; then
+      # grep for standard version
+      STANDARD_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}standard\s\(\K.*(?=\))/' Gemfile.lock)
+
+      # if standard version found, then pass it to the gem install
+      # left it empty otherwise, so no version will be passed
+      if [ -n "$STANDARD_GEMFILE_VERSION" ]; then
+        STANDARD_VERSION=$STANDARD_GEMFILE_VERSION
+      else
+        printf "Cannot get the standard's version from Gemfile.lock. The latest version will be installed."
+      fi
+    else
+      printf 'Gemfile.lock not found. The latest version will be installed.'
+    fi
+  else
+    # set desired standard version
+    STANDARD_VERSION=$INPUT_STANDARD_VERSION
+  fi
+
+  gem install -N standard --version "${STANDARD_VERSION}"
+
+  # Traverse over list of standard plugins
+  for plugin in $INPUT_STANDARD_PLUGINS; do
+    # grep for name and version
+    INPUT_STANDARD_PLUGIN_NAME=$(echo "$plugin" |awk 'BEGIN { FS = ":" } ; { print $1 }')
+    INPUT_STANDARD_PLUGIN_VERSION=$(echo "$plugin" |awk 'BEGIN { FS = ":" } ; { print $2 }')
+
+    # if version is 'gemfile'
+    if [ "${INPUT_STANDARD_PLUGIN_VERSION}" = "gemfile" ]; then
+      # if Gemfile.lock is here
+      if [ -f 'Gemfile.lock' ]; then
+        # grep for standard plugin version
+        STANDARD_PLUGIN_GEMFILE_VERSION=$(ruby -ne "print $& if /^\s{4}$INPUT_STANDARD_PLUGIN_NAME\s\(\K.*(?=\))/" Gemfile.lock)
+
+        # if standard plugin version found, then pass it to the gem install
+        # left it empty otherwise, so no version will be passed
+        if [ -n "$STANDARD_PLUGIN_GEMFILE_VERSION" ]; then
+          STANDARD_PLUGIN_VERSION=$STANDARD_PLUGIN_GEMFILE_VERSION
+        else
+          printf "Cannot get the standard plugin version from Gemfile.lock. The latest version will be installed."
+        fi
+      else
+        printf 'Gemfile.lock not found. The latest version will be installed.'
+      fi
+    else
+      # set desired standard plugin version
+      STANDARD_PLUGIN_VERSION=$INPUT_STANDARD_PLUGIN_VERSION
+    fi
+
+    # Handle plugins with no version qualifier
+    if [ -z "${STANDARD_PLUGIN_VERSION}" ]; then
+      unset STANDARD_PLUGIN_VERSION_FLAG
+    else
+      STANDARD_PLUGIN_VERSION_FLAG="--version ${STANDARD_PLUGIN_VERSION}"
+    fi
+
+    # shellcheck disable=SC2086
+    gem install -N "${INPUT_STANDARD_PLUGIN_NAME}" ${STANDARD_PLUGIN_VERSION_FLAG}
+  done
+  echo '::endgroup::'
+fi
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+if [ "${INPUT_USE_BUNDLER}" = "false" ]; then
+  BUNDLE_EXEC=""
+else
+  BUNDLE_EXEC="bundle exec "
+fi
+
+echo '::group:: Running standard with reviewdog 🐶 ...'
+# shellcheck disable=SC2086
+${BUNDLE_EXEC}standardrb ${INPUT_STANDARD_FLAGS} --require ${GITHUB_ACTION_PATH}/rdjson_formatter/rdjson_formatter.rb --format RdjsonFormatter \
+  | reviewdog -f=rdjson \
+      -name="${INPUT_TOOL_NAME}" \
+      -reporter="${INPUT_REPORTER}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS}
+
+reviewdog_rc=$?
+echo '::endgroup::'
+exit $reviewdog_rc
