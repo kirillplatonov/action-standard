f8fb08843cedf9cfac8b4fa307349ec97ec85aa7
Kirill Platonov <kirill@platonov.net>
2023-05-23 15:33:28 +0400

    Remove debug output

--- script.sh
+++ script.sh
@@ -93,8 +93,6 @@ fi
 echo '::group:: Running standard with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
 
-${BUNDLE_EXEC}standardrb ${INPUT_STANDARD_FLAGS} --require ${GITHUB_ACTION_PATH}/rdjson_formatter/rdjson_formatter.rb --format RdjsonFormatter
-
 ${BUNDLE_EXEC}standardrb ${INPUT_STANDARD_FLAGS} --require ${GITHUB_ACTION_PATH}/rdjson_formatter/rdjson_formatter.rb --format RdjsonFormatter \
   | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
